c465aba134d7e2c035ce273191082b9f6d6eaf53
Qrin <61843273+Qrinee@users.noreply.github.com>
2023-10-23 18:51:28 +0200

    optymalization

--- components/Grade.js
+++ components/Grade.js
@@ -4,84 +4,73 @@ import { Button, Card, Layout, Text, ButtonGroup } from '@ui-kitten/components';
 
 
 export const Grade = (props) => {
-    const average = (props.grades.reduce((acc, current) => acc + current, 0) / props.grades.length).toFixed(2)
-
-    const calcStatus = () => {
-        const difference = average - props.goal
-        if(props.goal == 0)
-            return ''
-        if(difference >= 0)
-            return 'success'
-        else if(difference >= -1)
-            return 'primary'
-        else if(difference >= -2)
-            return 'warning'
-        else
-            return 'danger'
-    }
-
-    return(
-    <>
-      <Layout
-        style={styles.topContainer}
-        level='1'
-      >
+    const { lesson, goal, grades } = props;
+  
+    const calculateAverage = () => {
+      return (grades.reduce((acc, current) => acc + current, 0) / grades.length).toFixed(2);
+    };
+  
+    const calculateStatus = () => {
+      const difference = calculateAverage() - goal;
+  
+      if (goal === 0) return '';
+      if (difference >= 0) return 'success';
+      if (difference >= -1) return 'primary';
+      if (difference >= -2) return 'warning';
+      return 'danger';
+    };
+  
+    const gradeButtons = {
+      1: { text: '1', color: '#D8304a' },
+      2: { text: '2', color: '#8123DB' },
+      3: { text: '3', color: '#CEDB23' },
+      4: { text: '4', color: '#23A1DB' },
+      5: { text: '5', color: '#23DB7B' },
+    };
+  
+    return (
+      <Layout style={styles.topContainer} level='1'>
         <Card
-          status={calcStatus()}
+          status={calculateStatus()}
           style={styles.card}
           header={
             <View>
-              <Text category='h6'>
-                {props.lesson}
-              </Text>
-              <Text category='s1'>
-                Średnia: {average}
-              </Text>
-              <Text category='s1'>
-                Cel: {props.goal == 0 ? 'brak' : props.goal}
-              </Text>
+              <Text category='h6'>{lesson}</Text>
+              <Text category='s1'>Średnia: {calculateAverage()}</Text>
+              <Text category='s1'>Cel: {goal === 0 ? 'brak' : goal}</Text>
             </View>
           }
         >
           <ButtonGroup style={styles.buttonGroup}>
-            {props.grades.map(e => {
-              switch (e) {
-                case 1:
-                  return <Button style={{ backgroundColor: '#D8304a' }}>1</Button>;
-                case 2:
-                  return <Button style={{ backgroundColor: '#8123DB' }}>2</Button>;
-                case 3:
-                  return <Button style={{ backgroundColor: '#CEDB23' }}>3</Button>;
-                case 4:
-                  return <Button style={{ backgroundColor: '#23A1DB' }}>4</Button>;
-                case 5:
-                  return <Button style={{ backgroundColor: '#23DB7B' }}>5</Button>;
-                default:
-                  return null;
-              }
-            })}
+            {grades.map((grade) => (
+              gradeButtons[grade] ? (
+                <Button key={grade} style={{ backgroundColor: gradeButtons[grade].color }}>
+                  {gradeButtons[grade].text}
+                </Button>
+              ) : null
+            ))}
           </ButtonGroup>
         </Card>
       </Layout>
-    </>
-    )
-    };
-
-const styles = StyleSheet.create({
-  topContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    marginTop: 5
-  },
-  card: {
-    flex: 1,
-    margin: 2,
-  },
-  footerContainer: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-  },
-  footerControl: {
-    marginHorizontal: 2,
-  },
-});
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    topContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginTop: 5
+    },
+    card: {
+      flex: 1,
+      margin: 2,
+    },
+    footerContainer: {
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+    },
+    footerControl: {
+      marginHorizontal: 2,
+    },
+  });
+  
